1440ed66241c8a87df58f71311d0441039b4a375
Marcus Francis <marcusmagnusson@outlook.com>
2018-11-19 12:41:54 +0100

    Feat: Scopes

---
+++ Scopes.js
@@ -0,0 +1,35 @@
+// Blocks within a function create a new lexical scope 
+
+// Global Scope
+
+// Global Scope starts under here 
+function aHero () {  // Establish global scope functions to return some words so the function calls in local scopes work
+	return "Gal";
+}
+function aFoil () {
+	return "Cow";
+}
+function aDeed () {
+	return "Taps";
+}
+
+var hero = aHero();
+var newSaga = function() { // <-- scope1 starts here
+    
+    var foil = aFoil();
+	var saga = function() { // <-- scope2 starts here 
+        
+        var deed = aDeed();
+		console.log(hero+deed+foil);
+	}; // <-- scope2 ends here 
+    
+    saga();
+	// -> GalTapsCow
+	saga();
+	// -> GalTapsCow
+}; // <-- scope1 ends here
+
+newSaga();
+// -> GalTapsCow
+newSaga();
+// -> GalTapsCow
